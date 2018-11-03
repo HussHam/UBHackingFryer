fbedcc4b8890569597f16fa7919f097c31f5b9fa
Daniel Shekhtman <dmshekht@buffalo.edu>
2018-11-03 14:39:45 -0400

    image upload added

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 var works = "works";
 var test = "test";
 
+function previewFile(){
+    var preview = document.querySelector('img'); //selects the query named img
+    var file    = document.querySelector('input[type=file]').files[0]; //same as here
+    var reader  = new FileReader();
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file); //reads the data as a URL
+    } else {
+        preview.src = "";
+    }
+}
+
+previewFile();  //calls the function named previewFile()
